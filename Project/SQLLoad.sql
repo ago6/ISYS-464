6dc42e0f73d27d1b69d81b02b5842101165f3f0f
Ale <agomez6@gmail.com>
2014-04-19 18:39:28 -0500

    a

---
+++ Project/SQLLoad.sql
@@ -0,0 +1,240 @@
+﻿INSERT INTO CUSTOMER VALUES
+('531', 'Alexia', 'L', 'Abundiz', '1 Coventry Ct.', 'Antioch', '94509', '(925) 310-9147',        'm5yp7u83pgndkc0@2en-dcsj0.com');
+INSERT INTO CUSTOMER VALUES
+('27723', 'Carla', 'C', 'Alameda', '100 Reef Dr.', 'Pittsburg', '94565', '(925) 341-4190', 'CCAlameda@yahoo.com');
+INSERT INTO CUSTOMER VALUES
+('473', 'Mariah', NULL, 'Ali', '2008 Monte Carlo Ct.', 'San Leandro', '94587', '(510) 308-5557', 'AliMarry@sfsu.edu');
+INSERT INTO CUSTOMER VALUES
+('155', 'Cory', NULL, 'Aquilar', '19 Midvale Dr.', 'Daly City', '94015', '(650) 296-6891', 'AquilarC@sfsu.edu');
+INSERT INTO CUSTOMER VALUES
+('30818', 'Lakita', NULL, 'Arrigo', '1933 Bosbury Way', 'Roseville', '95661', '(916) 302-1887', 'LakitaArrigo@gmail.com');
+INSERT INTO CUSTOMER VALUES
+('30971', 'Rozanne', 'S', 'Balthazor', '1026 10th St. Apt. B', 'Oakland', '94607', '(510) 459-2481',        'w_z62euv@yip57-oi.com');
+INSERT INTO CUSTOMER VALUES
+('475', 'Madeleine', NULL, 'Barroso', '2019 Lincoln Ave.#C', 'Alameda', '94501', '(510)  315-4311',        'ybnx6c3s1v@w-i2bizpz.com');
+INSERT INTO CUSTOMER VALUES
+('289716', 'Jerrell', 'S', 'Blackmore', '10282 Terry Way #4', 'Cupertino','95014','(650) 817-7242', 'pm8f-02q19@a2l5mqoaltd.com');
+INSERT INTO CUSTOMER VALUES
+('298', 'Vernita', NULL, 'Blansett', '2108 Stacia Way' 'San Francisco', '94117', '(415) 324-8255', 'VernBlansett@gmail.com');
+INSERT INTO CUSTOMER VALUES
+('804', 'Celia', NULL, 'Bodden', '212 Dorchester Ln.', 'Alamo', '94507', '(925) 325-3838', 'BoddenCelia@sfsu.edu');
+INSERT INTO CUSTOMER VALUES
+('147', 'John', NULL, 'Borgman', '2228 Ralmar St.', 'East Palo Alto', '94303', '(408) 350-1771', 'BorgmanJohn@sfsu.edu');
+INSERT INTO CUSTOMER VALUES
+('25359', 'Jeff', 'D', 'Bried', '282 Coral Reef  Way Apt. #4', 'Cupertino', '95014', '(650) 736-0995', 'JDBried@yahoo.com');
+INSERT INTO CUSTOMER VALUES
+('114', 'America', NULL,''Newbrough', '5922 13th St.', 'San Francisco', '94117', '(415) 694-8008', 'Newbrough.Amer@earthlink.net');
+INSERT INTO CUSTOMER VALUES
+('2523', 'Bronwyn', NULL, 'Briner', '2400 Mac Arthur Blvd. #305', 'Oakland', '94602', '(510) 350-8698',        'BrinerBronwyn@sfsu.edu');
+INSERT INTO CUSTOMER VALUES
+('592', 'Tisha', 'E', 'Capri', '1033 Fair Weather Dr.', 'Sacramento', '95832', '(916) 807-6567', 'Tisha.E.Capri@bluecross.net');
+
+
+
+
+
+
+INSERT INTO STORE VALUES
+('601', '3489 16th St.', 'San Francisco', '94114', '(415) 553-6888', '1600', 'Adrian', 'Peterson', '$7800');
+INSERT INTO STORE VALUES
+('249', '1650 Holloway Ave.', 'San Francisco', '94132', '(415) 841-0369', '1200', 'Trent', 'Richardson', '$5500');
+INSERT INTO STORE VALUES
+('567', '1780 Mendocino Ave.', 'Santa Rosa', '95401', '(707) 293-9814', '1400', 'Brittney',        'Griner', '$2500');
+INSERT INTO STORE VALUES
+('678', '21000 Stevens Creek Blvd.', 'Cupertino', '95014', '(408) 861-1000', '1500', 'Elena', 'Donne', '$3750');
+INSERT INTO STORE VALUES 
+('321', '2235 The Alameda', 'Santa Clara', '95050', '(408) 244-2034', '1800', 'Arian', 'Foster', '$3995');
+INSERT INTO STORE VALUES 
+('124', '475 Via Ortega', 'Stanford', '94305', '(650) 322‑1766', '1750', 'Skylar', 'Diggins', '$4400');
+INSERT INTO STORE VALUES
+('190', '2204 Broadway Street', 'Oakland', '94612', '(510) 338-6789', '1500', 'Marshawn', 'Lynch', '$4995');
+INSERT INTO STORE VALUES
+('789', '1822 N Main St', 'Salinas', '93906', '(831) 449-4000', '1450', 'Frank', 'Gore', '$2950'); 
+INSERT INTO STORE VALUES
+('899', '21 Railroad Ave', 'Danville', '94526', '(925) 820-9500', '1600', 'Darren', 'McFadden', '$2400');
+
+
+
+
+INSERT INTO MENUITEM VALUES
+('M001', 'Al Bundy', '$9.99', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M002', 'Backstabber', '$9.99', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M003','        Barry B.', '$8.98','        Meat');
+INSERT INTO MENUITEM VALUES
+('M004', 'Chipper's Fave','        $9.99', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M005', 'Doomsday', '$19.91', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M006', 'Elvis Kieth', '$8.98', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M007', 'Fat Bastard', '$8.98', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M008', 'Forty!??', '$11.11', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M009', 'Going Home For Thanksgiving', '$8.98', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M010', 'Hot Momma Huda', '$8.98', 'Meat');
+INSERT INTO MENUITEM VALUES
+('V001', 'Vegan Brutas', '$11.11', 'Vegan');
+INSERT INTO MENUITEM VALUES
+('S100', 'Coke', '$1.00', 'Soft Drink');
+INSERT INTO MENUITEM VALUES
+('S102', '7-Up', '$1.00', 'Soft Drink');
+INSERT INTO MENUITEM VALUES
+('S103','        Dr. Pepper', '$1.00', 'Soft Drink');
+INSERT INTO MENUITEM VALUES
+('S104', 'Diet Coke', '$1.00', 'Soft Drink');
+INSERT INTO MENUITEM VALUES
+('S105',        'Root Beer', '$1.00', 'Soft Drink');
+INSERT INTO MENUITEM VALUES
+('G001', 'Blind Date', '$11.11', 'Veggie');
+INSERT INTO MENUITEM VALUES
+('A202', 'Dirty Potato Chips', '$1.50', 'Side');
+INSERT INTO MENUITEM VALUES
+('A204', 'Oatmeal Cookie',        '$1.50', 'Side');
+INSERT INTO MENUITEM VALUES
+('M019', 'SF Giants', '$11.11', 'Meat');
+INSERT INTO MENUITEM VALUES
+('M013','Kryptonite','$19.91','Meat');
+INSERT INTO MENUITEM VALUES
+('M016','Nacho Boy','$11.11','Meat');
+
+
+
+
+INSERT INTO ORDERS VALUES
+('K3001', '$13.66', 'Delivery', '11:00:00', '2013-05-01', 'Cash', '298', '601');
+INSERT INTO ORDERS VALUES
+('K3002', '$8.98', 'Pickup',        '11:02:00', '2013-05-01', 'Cash', '475', '567');
+INSERT INTO ORDERS VALUES 
+('K3003', '$33.33', 'Pickup', '11:05:00', '2013-05-01',        'Credit Card', '30971', '190');
+INSERT INTO ORDERS VALUES
+('K3004', '$22.41', 'Pickup', '11:10:00', '2013-05-01', 'Debit', '114', '321');
+INSERT INTO ORDERS VALUES
+('K3005', '$12.61', 'Pickup', '11:15:00', '2013-05-01',        'Cash', '298', '124');
+INSERT INTO ORDERS VALUES
+('K3006', '$31.20', 'Pickup', '11:18:00', '2013-05-01', 'Debit', '155', '249');
+INSERT INTO ORDERS VALUES
+('K3007', '$25.32', 'Pickup', '11:19:00', '2013-05-01', 'Credit Card', '30818', '601');
+INSERT INTO ORDERS VALUES
+('K3008', '$92.18', 'Delivery', '11:20:00', '2013-05-01', 'Cash',        '2523', '190');
+INSERT INTO ORDERS VALUES 
+('K3009', '$12.49', 'Pickup', '11:21:00', '2013-05-01',        'Debit', '147', '789');
+INSERT INTO ORDERS VALUES
+('K3010', '$10.99', 'Pickup', '11:23:00', '2013-05-01', 'Cash', '592', '124');
+INSERT INTO ORDERS VALUES
+('K3011','$113.46', 'Delivery', '11:25:00', '2013-05-01', 'Cash','25359', '678');
+
+
+
+
+
+
+INSERT INTO ORDEREDITEMS VALUES
+('X5021','1', 'K3001', 'G001');
+INSERT INTO ORDEREDITEMS VALUES
+('X5021','1', 'K3001','G001');
+INSERT INTO ORDEREDITEMS VALUES
+('X5022','1','K3001','S104');
+INSERT INTO ORDEREDITEMS VALUES
+('X5023','1','K3001','A204');
+INSERT INTO ORDEREDITEMS VALUES
+('X5024','1','K3002','M007');
+INSERT INTO ORDEREDITEMS VALUES
+('X5025','3','K3003','M019');
+INSERT INTO ORDEREDITEMS VALUES
+('X5026','1','K3004','S104');
+INSERT INTO ORDEREDITEMS VALUES
+('X5027','1','K3004','A202');
+INSERT INTO ORDEREDITEMS VALUES
+('X5028','1','K3004','M103');
+INSERT INTO ORDEREDITEMS VALUES
+('X5029','1','K3005','M016');
+INSERT INTO ORDEREDITEMS VALUES
+('X5030','1','K3005','S015');
+INSERT INTO ORDEREDITEMS VALUES
+('X5031','1','K3006','M008');
+INSERT INTO ORDEREDITEMS VALUES
+('X5032','1','K3006','M010');
+INSERT INTO ORDEREDITEMS VALUES
+('X5033','1','K3006','V001');
+
+
+
+
+
+
+INSERT INTO DELIVERY VALUES
+('D9001','11:30:00', '2013-05-01','K3001');
+INSERT INTO DELIVERY VALUES
+('D9002','12:00:00', '2013-05-01','K3008');
+INSERT INTO DELIVERY VALUES
+('D9003','12:15:00','2013-05-01','K3011');
+INSERT INTO DELIVERY VALUES
+('D9004','12:50:00','2013-05-01','K3015');
+INSERT INTO DELIVERY VALUES
+('D9005','13:00:00','2013-05-01','K3016');
+INSERT INTO DELIVERY VALUES
+('D9006','16:33:00 ','2013-05-01','K3017');
+INSERT INTO DELIVERY VALUES
+('D9007','16:54:00','2013-05-01','K3018');
+INSERT INTO DELIVERY VALUES
+('D9008','18:32:00','2013-05-01','K3019');
+INSERT INTO DELIVERY VALUES
+('D9009','18:44:00','2013-05-01','K3020');
+INSERT INTO DELIVERY VALUES
+('D9010','11:36:00','2013-05-01','K3021');
+INSERT INTO DELIVERY VALUES
+('D9011','13:14:00','2013-05-01','K3022');
+INSERT INTO DELIVERY VALUES
+('D9012','15:14:00','2013-05-01','K3023');
+INSERT INTO DELIVERY VALUES
+('D9013','17:22:00','2013-05-01','K3027');
+INSERT INTO DELIVERY VALUES
+('D9014','18:08:00','2013-05-01','K3029');
+INSERT INTO DELIVERY VALUES
+('D9015','19:24:00','2013-05-01','K3031');
+INSERT INTO DELIVERY VALUES
+('D9016','13:03:00','2013-05-01','K3033');
+INSERT INTO DELIVERY VALUES
+('D9017', '13:46:00', '2013-05-01', 'K3035');
+INSERT INTO DELIVERY VALUES
+('D9018', '15:05:00', '2013-05-01', 'K3037');
+INSERT INTO DELIVERY VALUES
+('D9019', '18:27:00', '2013-05-01', 'K3039');
+INSERT INTO DELIVERY VALUES
+('D9020', '19:26:00', '2013-05-01', 'K3041');
+INSERT INTO DELIVERY VALUES
+('D9021', '18:35:00', '2013-05-01', 'K3043');
+INSERT INTO DELIVERY VALUES
+('D9022', '19:15:00', '2013-05-01', 'K3044');
+
+
+
+
+
+
+INSERT INTO PICKUP VALUES
+('P201', '11:20:00', '2013-05-01', 'K3002');
+INSERT INTO PICKUP VALUES
+('P202', '11:45:00', '2013-05-01', 'K3003');
+INSERT INTO PICKUP VALUES
+('P203', '11:30:00', '2013-05-01', 'K3004');
+INSERT INTO PICKUP VALUES
+('P204', '11:40:00', '2013-05-01', 'K3005');
+INSERT INTO PICKUP VALUES
+('P205', '11:50:00', '2013-05-01', 'K3006');
+INSERT INTO PICKUP VALUES
+('P206', '11:40:00', '2013-05-01', 'K3007');
+INSERT INTO PICKUP VALUES
+('P207', '11:42:00', '2013-05-01', 'K3009');
+INSERT INTO PICKUP VALUES
+('P208', '11:45:00', '2013-05-01', 'K3010');
+INSERT INTO PICKUP VALUES
+('P209', '12:00:00', '2013-05-01', 'K3012');
+INSERT INTO PICKUP VALUES
+('P210', '12:10:00', '2013-05-01', 'K3013');
+INSERT INTO PICKUP VALUES
+('P211', '12:15:00', '2013-05-01', 'K3014');
